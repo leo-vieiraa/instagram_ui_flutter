1d0fc7e12c3680958cf154a6db25b8bee02fe5ee
Leonardo Vieira <leonardo.vieira@br.experian.com>
2023-02-22 03:37:40 -0300

    created shop section

--- lib/pages/shop.dart
+++ lib/pages/shop.dart
@@ -1,13 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_ui_flutter/util/shop_grid.dart';
 
 class UserShop extends StatelessWidget {
   const UserShop({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('SHOP'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Shop',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              Row(
+                children: [
+                  Icon(Icons.calendar_today),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(Icons.menu),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Container(
+                padding: EdgeInsets.all(8),
+                color: Colors.grey[300],
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                      color: Colors.grey[500],
+                    ),
+                    Container(
+                      child: Center(
+                        child: Text(
+                          'Search',
+                          style: TextStyle(
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(child: ShopGrid()),
+        ],
       ),
     );
   }
